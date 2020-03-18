52dbe253b823329553b120ad41c9844a95775162
jcheron <myaddressmail@gmail.com>
2020-03-18 19:22:43 +0100

    add exercice 1

---
+++ td1/ex1.php
@@ -0,0 +1,5 @@
+<a href="?message=Hello">Afficher Hello</a>
+<a href="?message=Message">Afficher Hello</a>
+<a href="?message=Hello">Afficher Hello</a>
+<?php
+echo "Vous êtes sur la page n° " . $_GET["page"];
