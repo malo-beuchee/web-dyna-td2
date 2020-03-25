130d4b7c45b8f12aafd327164260a1986878cc8d
jcheron <myaddressmail@gmail.com>
2020-03-25 02:04:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 </head>
 <body>
   <h1>TD Web dynamique</h1>
-  <h2>TD n°1</h2>
+  <h2>Groupe de TD n°2</h2>
+  <h3>TD n°1</h3>
   <ul>
     <li><a href="td1/hello.php">Hello world</a></li>
     <li><a href="td1/ex1.php">Exercice 1</a></li>
