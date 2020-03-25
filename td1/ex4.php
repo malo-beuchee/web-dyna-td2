e918bbb9c4f7ff83062bf3e94b9b5842437323fa
jcheron <myaddressmail@gmail.com>
2020-03-25 09:51:04 +0100

    Suite exercice 4 récup params url

--- td1/ex4.php
+++ td1/ex4.php
@@ -1,5 +1,14 @@
-<?php $title='Exercice n°4';
+<a href="?l=2&c=2">Tableau 2 x 2</a><br>
+<a href="?l=5&c=5">Tableau 5 x 5</a><br>
+<a href="?l=10&c=8">Tableau 10 x 8</a><br>
+<hr>
+<?php
+$title='Exercice n°4 (Itérations)';
 include "include/header.php";
+//Récupère les paramètres l et c passés dans l'url
+$nbLignes=$_GET["l"]??10;
+$nbColonnes=$_GET["c"]??10;
+echo "<h2>Tableau de $nbLignes par $nbColonnes</h2>";
 ?>
 
 <?php
