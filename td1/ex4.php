9b654fb1b433a92fa34c742eaa523c5a7f2b44e0
jcheron <myaddressmail@gmail.com>
2020-03-25 11:07:35 +0100

    [ex4] Rouge pour 1 colonne sur 2
    C'est un peu moche, mais c'est l'exercice

--- td1/ex4.php
+++ td1/ex4.php
@@ -18,8 +18,10 @@ echo "<h2>Tableau de $nbLignes par $nbColonnes</h2>";
         $gras=($i%2==0)?"bold":"normal";
       ?>
         <tr>
-        <?php for($j=0;$j<$nbColonnes;$j++){?>
-            <td style="font-weight: <?=$gras ?>"><?="$i - $j"?></td>
+        <?php for($j=0;$j<$nbColonnes;$j++){
+            $couleur=($j%2==0)?"red":"black";
+            ?>
+            <td style="font-weight: <?=$gras ?>;color:<?=$couleur ?>"><?="$i - $j"?></td>
         <?php }?>
         </tr>
       <?php }?>
