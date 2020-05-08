09fe0f4e6ce72438dd119f07ad61a2408dd27285
beuchée <malo.beuche@gmail.com>
2020-05-08 22:51:04 +0200

    mise en page: exercise 2

--- td1/ex2.php
+++ td1/ex2.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Hello</title>
+	<head>
+		<meta charset="UTF-8">
+		<title>Hello</title>
 
-</head>
-<body>
-	
-<form method="GET">
-    <label for="size">Size : </label>
-    <input type="number" value="" name="size" id="size">
-    <input type="submit" value="Valider">
-    <label for="color"></label>
-    <input type="string" value="" name="color" id="size">
-    <input type="submit" value="Valider">
-</form>
-<?php
-		echo "<div style ='color :{$_GET["color"]}'style='font-size: {$_GET["size"]}px;'>Message de taille {$_GET["size"]}px en colour </div>";
-	?>
-</body>
+		</head>
+	<body>
+			
+		<form method="GET">
+		    <label for="size">Size : </label>
+		    <input type="number" value="" name="size" id="size">
+		    <input type="submit" value="Valider">
+		    <label for="color"></label>
+		    <input type="string" value="" name="color" id="size">
+		    <input type="submit" value="Valider">
+		</form>
+		<?php
+				echo "<div style ='color :{$_GET["color"]}'style='font-size: {$_GET["size"]}px;'>Message de taille {$_GET["size"]}px en colour </div>";
+			?>
+	</body>
 
 </html>
