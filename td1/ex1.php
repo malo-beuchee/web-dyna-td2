29015636a1bdb1dd83f57f2992b6f0ad0648e8c8
beuchée <malo.beuche@gmail.com>
2020-05-08 22:33:23 +0200

    mise en page: exercise 1

--- td1/ex1.php
+++ td1/ex1.php
@@ -1,19 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Hello</title>
-
-</head>
-<body>
-<?php
-#exemple d'addresse: http://127.0.0.1/tpmalo/tp1/exo1.php?page=1&name=malo&faire=jsp
-echo "vous etes sur la page ". $_GET["page"];
-echo "<br/>";	
-echo " Bonjour ". $_GET["name"];
-echo "<br/>";
-echo " Qu'est ce que tu fait ici ". $_GET["faire"];
-?>
-
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Hello</title>
+	</head>
+	<body>
+		<?php
+		#exemple d'addresse: http://127.0.0.1/tpmalo/tp1/exo1.php?page=1&name=malo&faire=jsp
+		echo "vous etes sur la page ". $_GET["page"];
+		echo "<br/>";	
+		echo " Bonjour ". $_GET["name"];
+		echo "<br/>";
+		echo " Qu'est ce que tu fait ici ". $_GET["faire"];
+		?>
+	</body>
 </html>
