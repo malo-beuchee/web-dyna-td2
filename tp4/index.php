6a2158c5e0b3d52d6f3fcc7336c4acb407ad9326
beuchée <malo.beuche@gmail.com>
2020-05-13 09:41:11 +0200

    ajout d'un bouton:index.php

--- tp4/index.php
+++ tp4/index.php
@@ -2,11 +2,11 @@
 include "header.php";
 echo "<br>"
 ?>
-<?php
-//création du message au dessus du bouton inscription
-echo "bienvenue!"; 
-echo '<br>';
-echo "cette application vous permet de vous inscrire à notre prochaine flashmob.";
-echo '<br>';
 
-?>
+<!--création du message au dessus du bouton inscription-->
+<class >
+<p>Bienvenue!</p>
+<br>
+<p>cette application vous permet de vous inscrire à notre prochaine flashmob.";</p>
+<br>
+<input type = "submit" value = "inscription" color = "green" onclick="inscription.php" >
